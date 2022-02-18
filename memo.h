d331ec74718485cee2de7fc837e9e28306b94308
GabrielCepeda <gdcg0001@ce.pucmm.edu.do>
2022-02-18 10:44:11 -0400

    first commit

---
+++ memo.h
@@ -0,0 +1,61 @@
+#ifndef __MEMO_H__
+
+#define __MEMO_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//#define XXMALLOC(a) xxmalloc(a, __LINE__)
+
+void *xxmalloc(size_t );
+size_t xxfree(void *);
+size_t infoReservado();
+size_t infoSize();
+void *xxcopy(void *);
+void memory_info(void *);
+
+typedef struct{
+   size_t size;
+   size_t referencia;
+}INFO;
+
+#define ITEM_TYPE_NONE    0
+#define ITEM_TYPE_STRING  1
+#define ITEM_TYPE_POINTER 2
+#define ITEM_TYPE_INTEGER 3
+#define ITEM_TYPE_ARRAY   4
+#define ITEM_TYPE_UNKNOWN 5
+
+//#define ITEM_IS_NONE( p ) (p)->type == ITEM_TYPE_NONE
+
+struct strItem;
+struct strBaseArray;
+
+typedef struct {
+   int type;
+   int refer;
+   size_t id;
+
+   union {
+      void *pointer;
+      int number;
+      char str[ 30 ];
+      struct strBaseArray* arr;
+   }item;
+
+} ITEM, *PITEM;
+
+typedef struct strItem{
+   PITEM pitem;
+   size_t index;
+
+}ITEM_ARRAY;
+
+typedef struct strBaseArray{
+   size_t length;
+   size_t capacity;
+   ITEM_ARRAY* item;
+
+}BASE_ARRAY;
+
+#endif
